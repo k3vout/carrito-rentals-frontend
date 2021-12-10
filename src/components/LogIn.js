138e0678f37e6313d454449c4dc3924382a29e53
Sebastián M <smunozmo@gmail.com>
2021-12-10 13:38:33 -0500

    Styles: Add styling to login page.

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { actions } from '../redux/app/app';
 import Alert from './modules/Alert';
 import SignUp from './SignUp';
+import splash from '../images/assets/carrito-splash.png';
 
 const LogIn = () => {
   const alert = useSelector((state) => state.alertReducer);
@@ -17,12 +18,24 @@ const LogIn = () => {
     setSignUpWindow(true);
   };
   return (
-    <div>
+    <div className="vh-100 text-center bg-warning d-flex flex-column justify-content-center">
+      <div>
+        <img
+          src={splash}
+          id="slide"
+          className="carrito-splash-pic"
+          alt="Carrito Logo"
+        />
+      </div>
       {alert ? <Alert message={alert} /> : false }
-      Username:
-      <input type="text" placeholder="Place you name here" onChange={(e) => setUsername(e.target.value)} />
-      <button type="button" onClick={() => { handleSubmit(); }}>Sign in</button>
-      <button type="button" onClick={() => { openSignUpPopUp(); }}>SignUp</button>
+      <div className="input-group mb-5 align-self-center username">
+        <span className="input-group-text">@</span>
+        <input type="text" placeholder="Username" onChange={(e) => setUsername(e.target.value)} className="form-control" />
+      </div>
+      <div className="input-group mb-5 align-self-center username text-center justify-content-center">
+        <button type="button" onClick={() => { handleSubmit(); }} className="btn btn-dark btn-sm mx-1">Log in</button>
+        <button type="button" onClick={() => { openSignUpPopUp(); }} className="btn btn-dark btn-sm mx-1">Sign Up</button>
+      </div>
       {signUpOpen ? <SignUp /> : false}
     </div>
   );
